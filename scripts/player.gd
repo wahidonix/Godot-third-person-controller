9594f5fe2fc96f4053e44d77249e43cc647dbb26
vahidrahic <wahidonix@gmail.com>
2024-01-26 17:11:21 +0100

    added some props to the map and an arena camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 var SPEED
 var isSprinting = false
+var isTPC = true
 
 @export_category("Movement variables")
 @export var walk_speed = 2.5
@@ -9,7 +10,9 @@ var isSprinting = false
 @export var JUMP_VELOCITY = 4.5
 @export_category("Camera variables")
 @export var sens = 0.5
-@export var pcam: PhantomCamera3D
+var pcam: PhantomCamera3D
+@export var thirdPersonCamera: PhantomCamera3D
+@export var arenaCamera: PhantomCamera3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -29,9 +32,10 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	animation_player.play("Idle")
 	SPEED = walk_speed
+	pcam = thirdPersonCamera
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and isTPC:
 		var pcam_rotation_degrees: Vector3
 		pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
 		pcam_rotation_degrees.x -= event.relative.y * sens
@@ -42,6 +46,10 @@ func _input(event):
 		#rotate_y(deg_to_rad(-event.relative.x * sens))
 
 func _physics_process(delta):
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -59,17 +67,18 @@ func _physics_process(delta):
 		isSprinting = false
 	
 	if Input.is_action_just_pressed("quit"):
-		get_tree().quit
+		get_tree().quit()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		var move_dir: Vector3 = Vector3.ZERO
 		move_dir.x = direction.x
 		move_dir.z = direction.z
 
-		move_dir = move_dir.rotated(Vector3.UP, pcam.get_third_person_rotation().y).normalized()
+		if isTPC:
+			move_dir = move_dir.rotated(Vector3.UP, pcam.get_third_person_rotation().y).normalized()
+		else:
+			move_dir = move_dir.rotated(Vector3.UP, pcam.rotation.y).normalized()
 		velocity.x = move_dir.x * SPEED
 		velocity.z = move_dir.z * SPEED
 		
@@ -85,3 +94,25 @@ func _physics_process(delta):
 			animation_player.play("Idle")
 
 	move_and_slide()
+
+func changeCamera():
+	if pcam == thirdPersonCamera:
+		thirdPersonCamera.set_priority(0)
+		arenaCamera.set_priority(1)
+		isTPC = false
+		pcam = arenaCamera
+	else:
+		thirdPersonCamera.set_priority(1)
+		arenaCamera.set_priority(0)
+		isTPC = true
+		pcam = thirdPersonCamera
+
+
+func _on_area_3d_body_entered(body):
+	if body.name == "Player":
+		changeCamera()
+
+
+func _on_area_3d_body_exited(body):
+	if body.name == "Player":
+		changeCamera()
